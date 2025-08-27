cf6df80b1a080c5765bc9819f632f1780bcf1363
diogo-adao <diogoadao01x@gmail.com>
2025-08-27 02:31:05 +0100

    module 1 ready

--- cpp_module01/ex04/main.cpp
+++ cpp_module01/ex04/main.cpp
@@ -1,24 +1,37 @@
-#include <fstream>
 #include <iostream>
+#include <fstream>
 #include <string>
 
 int main(int argc, char *argv[])
 {
-    (void)argv;
     if (argc != 4) {
         std::cout << "Usage: ./file <filename> <s1> <s2>" << std::endl;
         return (1);
     }
 
-    std::string filename = "example.txt";
-    std::ifstream infile(filename);
+    std::string filename = argv[1];
+    std::ifstream infile(filename.c_str());
     if (!infile.is_open()) {
         std::cerr << "Error: could not open file " << filename << std::endl;
         return 1;
     }
+    std::ofstream outfile((filename + ".replace").c_str());
+
     std::string line;
+    std::string s1 = argv[2];
+    std::string s2 = argv[3];
     while (std::getline(infile, line)) {
-        std::cout << line << std::endl;
-}
+        size_t pos = 0;
+        while ((pos = line.find(s1, pos)) != std::string::npos)
+        {
+            line.erase(pos, s1.length());
+            line.insert(pos, s2);
+            pos += s2.length();
+        }
+        outfile << line << "\n"; 
+    }
+
+    infile.close();
+    outfile.close();
     return (0);
 }
---
+++ cpp_module01/ex05/main.cpp
@@ -0,0 +1,11 @@
+#include "Harl.hpp"
+
+int main() {
+    Harl h;
+    h.complain("DEBUG");
+    h.complain("INFO");
+    h.complain("WARNING");
+    h.complain("ERROR");
+    h.complain("BLAH");
+    return (0);
+}
