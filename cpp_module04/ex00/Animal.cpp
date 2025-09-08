c3541c9e64b76f43d3b4ecd14079a07daf205d21
diogo-adao <diogoadao01x@gmail.com>
2025-09-08 17:41:01 +0100

    started module 4

---
+++ cpp_module04/ex00/Animal.cpp
@@ -0,0 +1,25 @@
+#include "Animal.hpp"
+
+Animal::Animal()
+{
+	std::cout << "[ANIMAL] Default Constructor Called" << std::endl;
+}
+
+Animal::Animal(const Animal& original)
+{
+	std::cout << "[ANIMAL] Copy Constructor Called" << std::endl;
+	*this = original;
+}
+
+Animal& Animal::operator=(const Animal& original)
+{
+	std::cout << "[ANIMAL] Copy Assignment Operator Called" << std::endl;
+	if (this != &original)
+		*this = original;
+	return (*this);
+}
+
+Animal::~Animal()
+{
+	std::cout << "[ANIMAL] Destructor Called" << std::endl;
+}
